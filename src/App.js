2bdd1fdc284a13fece1be1dda734546f65bd09aa
sergiocohens <sergiocohensalama@pursuit.org>
2020-03-07 17:43:15 -0500

    basic bootstraping

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Switch, Route, Link } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
   return (
     <div className="App">
-    <Navbar>
-      <Navbar.Brand>Sergio Cohen-Salama</Navbar.Brand>
-    </Navbar>
+    <Container fluid='true'>
+      <Navbar sticky='top' bg='light'>
+        <Container>
+        <Nav>
+          <Nav.Link>
+            <Link to='/personal-portfolio-website'>Home</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to='/personal-portfolio-website/about'>About</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to='/personal-portfolio-website/contact'>Contact{' '}</Link>
+          </Nav.Link>
+        </Nav>
+        <Navbar.Brand>Sergio Cohen-Salama</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+    <Switch>
+      <Route exact path='/personal-portfolio-website' component={Home}/>
+      <Route path='/personal-portfolio-website/about' component={About}/>
+      <Route path='/personal-portfolio-website/contact' component={Contact}/>
+    </Switch>
     </div>
   );
 }
