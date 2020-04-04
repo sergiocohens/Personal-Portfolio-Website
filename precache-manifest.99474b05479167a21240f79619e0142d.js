33e8e883f72ff369885f6ed231dec1b56f6d79f8
cohen-salama <sergiocohensalama@pursuit.org>
2020-04-04 15:14:50 -0400

    Updates

---
+++ precache-manifest.99474b05479167a21240f79619e0142d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc48f3ea779c896e4c953296034230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d94995f55fdd84e28d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "73e4859592acdfb279dd",
+    "url": "/static/css/main.618deeeb.chunk.css"
+  },
+  {
+    "revision": "c6d94995f55fdd84e28d",
+    "url": "/static/js/2.b0c7ccce.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.b0c7ccce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e4859592acdfb279dd",
+    "url": "/static/js/main.9094b7b9.chunk.js"
+  },
+  {
+    "revision": "a1e3a48a678a775131ef",
+    "url": "/static/js/runtime-main.891e2277.js"
+  },
+  {
+    "revision": "c920c88a794037df58acd789ccf7a026",
+    "url": "/static/media/c-s.c920c88a.jpg"
+  },
+  {
+    "revision": "b6e70d831f5980321fa7e738bdd0575d",
+    "url": "/static/media/clap.b6e70d83.mp3"
+  },
+  {
+    "revision": "ca07d05da58deb01db96e1574591a640",
+    "url": "/static/media/greenlist.ca07d05d.png"
+  },
+  {
+    "revision": "61cc40e2e1f8bb36d4ad342d778fa951",
+    "url": "/static/media/hihat.61cc40e2.mp3"
+  },
+  {
+    "revision": "6fd87e4f3340454cb898eb15e83e4c97",
+    "url": "/static/media/kick.6fd87e4f.mp3"
+  },
+  {
+    "revision": "4bed346fec8b281d3d0a50bdce4d89c0",
+    "url": "/static/media/nuncaestuveaqui.4bed346f.jpg"
+  },
+  {
+    "revision": "0fa1bf1f71e1c4d298ed30cbff22d7e7",
+    "url": "/static/media/snare.0fa1bf1f.mp3"
+  }
+]);
