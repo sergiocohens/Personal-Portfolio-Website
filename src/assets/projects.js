f94c974829cf318fdd45ab0d3040b89401583d21
cohen-salama <sergiocohensalama@pursuit.org>
2020-04-04 15:04:07 -0400

    update projects links

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,15 +5,15 @@ const project1 = {
   name: 'Greenlist',
   image: greenlistPic,
   body: 'An app that makes getting eco-friendly materials easy for designers.',
-  github: 'https://github.com/joseph-p-pasaoa/greenlistRegistry_front__Web',
-  heroku: ''
+  github: 'https://github.com/joseph-p-pasaoa/greenlistRegistry__FSWeb',
+  heroku: 'https://greenlist-registry.herokuapp.com'
 }
 
 const project2 = {
   name: 'vinyl effect',
   image: vinyleffectPic,
   body: 'A desktop-browser music app with seamless song transitions.',
-  github: 'https://github.com/sergiocohens/Music-Player-1',
+  github: 'https://github.com/cohen-salama/vinyl-effect',
   heroku: 'https://nunca-estuve-aqui.herokuapp.com/'
 }
 
